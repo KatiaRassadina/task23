93d5dd994c38c83550e4470d5085559242163432
KatiaRassadina <114443286+KatiaRassadina@users.noreply.github.com>
2022-10-23 23:16:20 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N  
+Console.WriteLine("Input an inger number");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+for(int a = 1; a <= n; a++);
+{
+    index = (int)Math.Pow(a, 3);
+}
+Console.WriteLine(index + " ");
